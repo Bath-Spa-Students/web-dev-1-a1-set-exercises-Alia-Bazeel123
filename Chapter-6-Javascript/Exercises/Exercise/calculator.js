2d78299593436c5f7d45a7038968ad628babe3aa
Alia bint Bazeel <aliabazeel123@gmail.com>
2024-04-04 04:17:58 +0400

    exercise

---
+++ Chapter-6-Javascript/Exercises/Exercise/calculator.js
@@ -0,0 +1,9 @@
+
+function totalCost() {
+    const cpl = document.getElementsById('cpl').value;
+    const nlp = document.getElementsById('nlp').value;
+    const tc = cpl * nlp;
+    document.getElementsById('tc').innerText = 'Total Cost: $${tc.toFixed(2)}';
+}
+
+document.getElementsById('cal').addEventListener('click',CalculateTheTotalCost);
